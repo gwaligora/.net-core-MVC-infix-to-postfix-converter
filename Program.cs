0907b99236651a5fed70d215e4e38a63437d3d95
gwaligora <g.waligora001@gmail.com>
2020-03-04 15:29:38 +0100

    xd

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
